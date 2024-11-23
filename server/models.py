d021a9045d609e8bf0c9fc6d3d2143c670e9198d
innocent <iparainnocent@gmail.com>
2024-11-23 09:15:33 +0300

    commit changes

--- server/models.py
+++ server/models.py
@@ -3,30 +3,62 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
-
-class Customer(db.Model):
-    __tablename__ = 'customers'
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship("Review", back_populates="customer", cascade="all, delete-orphan")
+    items = association_proxy("reviews", "item", creator=lambda item: Review(item=item))
+
+    serialize_rules = ("-reviews.customer",)  # Exclude reviews when serializing customer
+
     def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+        return f"<Customer {self.id}, {self.name}>"
 
-
-class Item(db.Model):
-    __tablename__ = 'items'
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
+    
+    reviews = db.relationship("Review", back_populates="item", cascade="all, delete-orphan")
+    customers = association_proxy(
+        "reviews",
+        "customer",
+        creator=lambda customer_obj: Review(customer=customer_obj),
+    )
+
+    serialize_rules = ("-reviews.item",)  # Exclude reviews when serializing item
 
     def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+        return f"<Item {self.id}, {self.name}, {self.price}>"
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)  # Ensure comment is not null
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
+    
+    customer = db.relationship("Customer", back_populates="reviews")
+    item = db.relationship("Item", back_populates="reviews")
+
+    serialize_rules = (
+        "-customer.reviews",  # Exclude reviews when serializing customer
+        "-item.reviews",      # Exclude reviews when serializing item
+    )
+
+    def __repr__(self):
+        return f"<Review {self.id}, Comment: {self.comment}, Customer ID: {self.customer_id}, Item ID: {self.item_id}>"
